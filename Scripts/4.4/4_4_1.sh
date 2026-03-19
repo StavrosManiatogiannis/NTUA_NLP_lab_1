fe387d9e02d3c087a25670da6a6a7664f4165135
StavrosManiatogiannis <168526227+StavrosManiatogiannis@users.noreply.github.com>
2026-03-19 15:01:03 +0200

    Update 4_4_1.sh

--- Scripts/4.4/4_4_1.sh
+++ Scripts/4.4/4_4_1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/kaldi/egs/usc
-steps/train_mono.sh --boost_silence 1.25 --nj 4 --cmd "$train_cmd" data/train data/lang exp/mono
+steps/train_mono.sh --boost_silence 1.25 --nj 4 --cmd "utils/run.pl" data/train data/lang exp/mono
