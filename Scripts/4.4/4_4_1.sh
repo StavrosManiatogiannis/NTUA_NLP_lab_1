3aa214e0591995c44d7d5cff2574fb0d4d1d8d7d
Stavros Maniatogiannis <168526227+StavrosManiatogiannis@users.noreply.github.com>
2026-03-21 10:09:27 +0200

    Update 4_4_1.sh
    clean up

--- Scripts/4.4/4_4_1.sh
+++ Scripts/4.4/4_4_1.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd ~/kaldi/egs/usc
-steps/train_mono.sh --boost_silence 1.25 --nj 4 --cmd "utils/run.pl" data/train data/lang exp/mono
+
+. ./path.sh
+. ./cmd.sh
+
+
+steps/train_mono.sh --boost_silence 1.25 --nj 4 --cmd "$train_cmd" data/train data/lang exp/mono
