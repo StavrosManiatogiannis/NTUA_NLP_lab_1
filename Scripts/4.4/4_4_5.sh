eb00ac3db893cb990ffce2b74ef02290b267d952
Stavros Maniatogiannis <168526227+StavrosManiatogiannis@users.noreply.github.com>
2026-03-21 10:13:59 +0200

    Update 4_4_5.sh
    clean up

--- Scripts/4.4/4_4_5.sh
+++ Scripts/4.4/4_4_5.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 cd ~/kaldi/egs/usc
 
+. ./path.sh
+. ./cmd.sh
 
-steps/align_si.sh --nj 4 --cmd "utils/run.pl" data/train data/lang exp/mono exp/mono_ali
+
+
+steps/align_si.sh --nj 4 --cmd "$train_cmd" data/train data/lang exp/mono exp/mono_ali
 
 
 
-steps/train_deltas.sh --cmd "utils/run.pl" 2000 11000 data/train data/lang  exp/mono_ali exp/tri
+steps/train_deltas.sh --cmd "$train_cmd" 2000 11000 data/train data/lang  exp/mono_ali exp/tri
 
 
 
@@ -17,12 +21,12 @@ utils/mkgraph.sh data/lang_test_bg exp/tri exp/tri/graph_bg
 
 
 
-steps/decode.sh --nj 4 --cmd run.pl exp/tri/graph_ug data/dev exp/tri/dev_decoded_ug
-steps/decode.sh --nj 4 --cmd run.pl exp/tri/graph_ug data/test exp/tri/test_decoded_ug
+steps/decode.sh --nj 4 --cmd "$decode_cmd" exp/tri/graph_ug data/dev exp/tri/dev_decoded_ug
+steps/decode.sh --nj 4 --cmd "$decode_cmd" exp/tri/graph_ug data/test exp/tri/test_decoded_ug
 
 
-steps/decode.sh --nj 4 --cmd run.pl exp/tri/graph_bg data/dev exp/tri/dev_decoded_bg
-steps/decode.sh --nj 4 --cmd run.pl exp/tri/graph_bg data/test exp/tri/test_decoded_bg
+steps/decode.sh --nj 4 --cmd "$decode_cmd" exp/tri/graph_bg data/dev exp/tri/dev_decoded_bg
+steps/decode.sh --nj 4 --cmd "$decode_cmd" exp/tri/graph_bg data/test exp/tri/test_decoded_bg
 
 
 
