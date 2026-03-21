d12eedc98f1c702d16731b1fe4320d3e58312ad4
Stavros Maniatogiannis <168526227+StavrosManiatogiannis@users.noreply.github.com>
2026-03-21 10:12:19 +0200

    Update 4_4_4.sh
    clean up

--- Scripts/4.4/4_4_4.sh
+++ Scripts/4.4/4_4_4.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 cd ~/kaldi/egs/usc
 
+. ./path.sh
+. ./cmd.sh
 
+# scoring dev and test for unigram mono
 local/score.sh --cmd "utils/run.pl" data/dev exp/mono/graph_ug exp/mono/dev_decoded_ug
 local/score.sh --cmd "utils/run.pl" data/test exp/mono/graph_ug exp/mono/test_decoded_ug
 
-
+# scoring dev and test for bigram mono
 local/score.sh --cmd "utils/run.pl" data/dev exp/mono/graph_bg exp/mono/dev_decoded_bg
 local/score.sh --cmd "utils/run.pl" data/test exp/mono/graph_bg exp/mono/test_decoded_bg
