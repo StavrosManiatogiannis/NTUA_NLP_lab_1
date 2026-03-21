64872d98cee9a7fd2e7d37425092116341dc4bd1
Stavros Maniatogiannis <168526227+StavrosManiatogiannis@users.noreply.github.com>
2026-03-21 10:11:25 +0200

    Update 4_4_3.sh
    clean up

--- Scripts/4.4/4_4_3.sh
+++ Scripts/4.4/4_4_3.sh
@@ -2,14 +2,16 @@
 sudo ln -s $(which python3) /usr/bin/python
 cd ~/kaldi/egs/usc
 . ./path.sh
+. ./cmd.sh
 
 if [ ! -x ~/kaldi/egs/usc/local/score.sh ]; then
     chmod +x ~/kaldi/egs/usc/local/score.sh
 fi
 
-steps/decode.sh --nj 4 --cmd run.pl exp/mono/graph_ug data/dev exp/mono/dev_decoded_ug
-steps/decode.sh --nj 4 --cmd run.pl exp/mono/graph_ug data/test exp/mono/test_decoded_ug
+# decode dev and test for unigram mono model
+steps/decode.sh --nj 4 --cmd "$decode_cmd" exp/mono/graph_ug data/dev exp/mono/dev_decoded_ug
+steps/decode.sh --nj 4 --cmd "$decode_cmd" exp/mono/graph_ug data/test exp/mono/test_decoded_ug
 
-
-steps/decode.sh --nj 4 --cmd run.pl exp/mono/graph_bg data/dev exp/mono/dev_decoded_bg
-steps/decode.sh --nj 4 --cmd run.pl exp/mono/graph_bg data/test exp/mono/test_decoded_bg
+# decode dev and test for bigram mono model
+steps/decode.sh --nj 4 --cmd "$decode_cmd" exp/mono/graph_bg data/dev exp/mono/dev_decoded_bg
+steps/decode.sh --nj 4 --cmd "$decode_cmd" exp/mono/graph_bg data/test exp/mono/test_decoded_bg
